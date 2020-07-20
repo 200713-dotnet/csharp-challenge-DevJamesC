0eed0a5a3d52c797331af1c5a1f17c448342183c
James Crescenzi <jkc36@pitt.edu>
2020-07-20 16:44:43 -0400

    Completed challange

--- Palindrome.Testing/UnitTest1.cs
+++ Palindrome.Testing/UnitTest1.cs
@@ -1,13 +1,26 @@
 using System;
 using Xunit;
 
+
+
 namespace Palindrome.Testing
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+      [Theory]
+        [InlineData("Mary")]
+        [InlineData("Donnilas")]
+        [InlineData("Renal")]
+        [InlineData("Damad")]
+        [InlineData("Goog")]
+        [InlineData("Beneb")]
+        public void Test1(string s)
         {
+            Palindrome.Domain.Class1 sut = new Palindrome.Domain.Class1();
+
+            bool b=sut.TestPal(s);
+
+            Assert.True(b);
 
         }
     }
