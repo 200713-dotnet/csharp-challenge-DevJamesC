4ee5afbdbd6c68674ce68205245d81d2bca8ae19
James Crescenzi <jkc36@pitt.edu>
2020-07-20 17:12:57 -0400

    please work

--- Palindrome.Testing/TestTestPalindrome.cs
+++ Palindrome.Testing/TestTestPalindrome.cs
@@ -8,18 +8,19 @@ namespace Palindrome.Testing
     public class TestTestPalindrome
     {
       [Theory]
-        [InlineData("Mary")]
-        [InlineData("Donnilas")]
-        [InlineData("Renal")]
-        [InlineData("Damad")]
-        [InlineData("Goog")]
-        [InlineData("Beneb")]
+        [InlineData("Mary")] //should fail
+        [InlineData("Donnilas")] //should fail
+        [InlineData("Renal")] //should fail
+        [InlineData("Damed")] //should pass
+        [InlineData("G@@g")] //should pass (does not though)
+        [InlineData("Beneb")] //should pass
         public void Test1(string s)
         {
+            //arrange
             Palindrome.Domain.TestPalindrome sut = new Palindrome.Domain.TestPalindrome();
-
+            //act
             bool b=sut.TestPal(s);
-
+            //assert
             Assert.True(b);
 
         }
